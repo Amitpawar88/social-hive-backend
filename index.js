7d68bd05f51569b64c49089130ee8e9394a2757c
amitpawar <amitpawar7475@gmail.com>
2024-05-19 14:47:06 +0530

    initial setup: login, register, logout apis, mongodb connection

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+
+dotenv.config({
+    path: ".env"
+})
+databaseConnection();
+const app = express();
+
+//middlewares
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+//APIs
+app.use("/api/v1/user", userRoute);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listen at port ${process.env.PORT}`);
+})
